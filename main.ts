c0e16c10c13be338cd7ad4322384da1de8a8f9dc
Rizwan shar <160394505+rizwansharbaloch@users.noreply.github.com>
2024-06-20 22:31:45 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log(" welcome at code with Rizwan ahmed");
+
+const randomNumber = 16;
+
+
+const answers = await inquirer.prompt([
+{
+    name: "userGuessedNumber",
+    type: "number",
+     message: "please guess a number",
+ },
+
+ ]);
+    
+     if (answers.userGuessedNumber === randomNumber){
+        
+         console.log("congratulation ! you guess a correct number.");
+     }
+     else{
+         console.log("sorry, you guess a wrong number");
+
+     }
